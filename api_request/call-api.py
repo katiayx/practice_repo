cb2a554787f05e69f0d7fa8963fe295f3737feac
Katia Wu <katiayx@gmail.com>
2016-12-10 22:40:13 -0800

    working on displaying 1 movie's details using flask and jinja

--- api_request/call-api.py
+++ api_request/call-api.py
@@ -4,12 +4,20 @@ import requests
 
 movie_db_api = os.environ["movie_db_api"]
 
-MOVIEDB = "https://api.themoviedb.org/3/discover/movie?api_key=" + movie_db_api + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2016-12-04&primary_release_date.lte=2016-12-06&with_release_type=3%7C2&with_original_language=en"
+MOVIEDB = "https://api.themoviedb.org/3/movie/550?api_key=" + movie_db_api
 
 
-response = requests.get(MOVIEDB)
-r_dict = response.json()
+def call_api():
+	"""get details for 1 movie"""
 
-for i in r_dict['results']:
-    movie_name = i.get('title')
-    print movie_name
+	response = requests.get(MOVIEDB)
+	r_dict = response.json()
+	
+	title = r_dict['original_title']
+	poster = r_dict['poster_path']
+	synopsis = r_dict['overview']
+	ave_rating = r_dict['vote_average']
+
+	return title, poster, synopsis, ave_rating
+
+
