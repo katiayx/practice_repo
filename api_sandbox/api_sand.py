f77701bec236f2b978db37e0808d24402fb6d3d2
Katia Wu <katiayx@gmail.com>
2016-12-16 06:49:44 +0000

    displayed movie details on localhost

--- api_sandbox/api_sand.py
+++ api_sandbox/api_sand.py
@@ -6,18 +6,16 @@ movie_db_api = os.environ["movie_db_api"]
 
 MOVIEDB = "https://api.themoviedb.org/3/movie/550?api_key=" + movie_db_api
 
-
 def call_api():
 	"""get details for 1 movie"""
 
 	response = requests.get(MOVIEDB)
 	r_dict = response.json()
-	
-	title = r_dict['original_title']
-	poster = r_dict['poster_path']
-	synopsis = r_dict['overview']
-	ave_rating = r_dict['vote_average']
 
-	return title, poster, synopsis, ave_rating
+	movie_details = {}
+	movie_details['title'] = r_dict['original_title']
+	movie_details['poster'] = r_dict['poster_path']
+	movie_details['synopsis'] = r_dict['overview']
+	movie_details['ave_rating'] = r_dict['vote_average']
 
-
+	return movie_details
--- api_sandbox/server.py
+++ api_sandbox/server.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
+from flask import (Flask, render_template, redirect, request, jsonify)
+
+from jinja2 import StrictUndefined
 
 from api_sand import call_api
 
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def display_details():
 	"""call helper function to get details on 1 movie"""
 
-	call_api()
-	title = request.args.form()
+	movie_details = call_api()
 
+	title = movie_details['title']
+	poster = movie_details['poster']
+	synopsis = movie_details['synopsis']
+	ave_rating = movie_details['ave_rating']
 
 
 	return render_template('details.html',
