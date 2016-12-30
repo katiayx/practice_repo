4120622b1fbcc95d89c8ea18bf3c7de2c9adec97
Katia Wu <katiayx@gmail.com>
2016-12-29 17:14:26 -0800

    used string formatting to insert API key: call success

--- events_sandbox/event-api.py
+++ events_sandbox/event-api.py
@@ -5,14 +5,12 @@ import requests
 EB_TOKEN = os.environ["EB_TOKEN"]
 payload = {"q": "python", "sort_by": "date", "start_date.keyword": "next_week"}
 search_url = "https://www.eventbriteapi.com/v3/events/search/"
+headers = {'Authorization':'Bearer %s' % EB_TOKEN}
 
 def call_api():
 	"""try eventbrite api"""
 
-	response = requests.get(search_url,params=payload,
-		headers = {"Authorization": "Bearer" + EB_TOKEN},
-		verify = True,
-	)
+	response = requests.get(search_url,params=payload,headers=headers)
 
 	r_dict = response.json()
 
